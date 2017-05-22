592cc8ffa80a7b15d50a8856dffaf02edcbddcff
Magnus Weidmar <Giraffen@DESKTOP-2GM47HH>
2017-05-22 11:39:09 +0200

    Excercie1 done

---
+++ TDD/ValidatorEngine.Test/ValidationTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorEngine.Test
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+        [Test]
+        public void TrueForValidAdress()
+        {
+            var sut = new Validator();
+
+            bool isValid = sut.ValidateEmailAdress("weidmar@gmail.com");
+            
+            Assert.IsTrue(isValid);
+        }
+
+        [Test]
+        public void FalseForInvalidAdress()
+        {
+            var sut = new Validator().ValidateEmailAdress("Hepp");
+
+            Assert.IsFalse(sut);
+        }
+    }
+}
---
+++ TDD/ValidatorEngine/Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorEngine
+{
+    public class Validator
+    {
+        public bool ValidateEmailAdress(string emailToValidate)
+        {
+            try
+            {
+                var addr = new MailAddress(emailToValidate);
+                return addr.Address == emailToValidate;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
